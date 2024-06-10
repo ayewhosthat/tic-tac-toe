59d5be5c5930149cc3fdf03f33c2fa4d562498ce
Alan Hu <alan.notiverson@gmail.com>
2024-06-10 14:49:10 -0400

    Edit document name + change what is returned in GameController

--- main.js
+++ main.js
@@ -131,7 +131,7 @@ function GameController() {
     };
 
 
-    return {board, player1, player2, printNewRound, playRound, playGame};
+    return {printNewRound, playRound, playGame};
 }
 
 const game = GameController();
