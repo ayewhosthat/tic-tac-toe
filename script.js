e316e27daea7c9411390827411037e5ecb86f7c0
Alan Hu <alan.notiverson@gmail.com>
2024-06-10 22:43:59 -0400

    Extract player names from modal

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
+const form = document.querySelector('form');
 const modal = document.querySelector('.modal');
 modal.showModal();
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const p1Name = document.getElementById('p1name').value;
+    const p2Name = document.getElementById('p2name').value;
+    console.log(p1Name);
+    console.log(p2Name);
+});
